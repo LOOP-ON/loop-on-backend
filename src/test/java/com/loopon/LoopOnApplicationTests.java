25fe7e6fc39c3f3f557bbeb994b3b4ca97ca974d
Seungwon-Choi <bestwondev@gmail.com>
2025-12-25 23:59:29 +0900

    chore: spring boot 프로젝트 추가

---
+++ src/test/java/com/loopon/LoopOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
