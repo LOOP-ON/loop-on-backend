2a7af023b119511b99acd721d711d4e8e6be3906
Seungwon-Choi <bestwondev@gmail.com>
2025-12-26 22:25:39 +0900

    feat: JwtTokenProvider, JwtTokenValidator 추가

---
+++ src/main/java/com/loopon/global/security/principal/PrincipalDetails.java
@@ -0,0 +1,86 @@
+package com.loopon.global.security.principal;
+
+import com.loopon.user.domain.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Getter
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private final User user;
+    private final Map<String, Object> attributes;
+
+    private PrincipalDetails(User user) {
+        this.user = user;
+        this.attributes = Map.of();
+    }
+
+    private PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    public static PrincipalDetails of(User user) {
+        return new PrincipalDetails(user);
+    }
+
+    public static PrincipalDetails of(User user, Map<String, Object> attributes) {
+        return new PrincipalDetails(user, attributes);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getName() {
+        return user.getEmail();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+}
