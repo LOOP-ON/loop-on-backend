272c269959b70c5498a4dec0fa812e1558f7bd92
Seungwon-Choi <bestwondev@gmail.com>
2025-12-26 21:47:16 +0900

    feat: User 엔티티 클래스 추가

---
+++ src/main/java/com/loopon/user/domain/User.java
@@ -0,0 +1,40 @@
+package com.loopon.user.domain;
+
+import com.loopon.global.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    public static User of(Long id, String email) {
+        return User.builder()
+                .id(id)
+                .email(email)
+                .build();
+    }
+}
