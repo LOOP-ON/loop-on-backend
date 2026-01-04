9dd775fd44c6c587b8077aa9b2d593751098f353
박창현 <changhyeonb533@gmail.com>
2026-01-04 21:02:41 +0900

    test : Add integration tag to LoopOnApplicationTests

--- src/test/java/com/loopon/LoopOnApplicationTests.java
+++ src/test/java/com/loopon/LoopOnApplicationTests.java
@@ -3,6 +3,7 @@ package com.loopon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("integration")
 @SpringBootTest
 class LoopOnApplicationTests {
 
