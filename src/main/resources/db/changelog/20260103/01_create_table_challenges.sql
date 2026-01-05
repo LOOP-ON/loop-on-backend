8ce770cdc897b8737d31d570942521e504799b53
Seungwon-Choi <bestwondev@gmail.com>
2026-01-05 20:56:09 +0900

    refactor: challenges 테이블 외래키 제거

--- src/main/resources/db/changelog/20260103/01_create_table_challenges.sql
+++ src/main/resources/db/changelog/20260103/01_create_table_challenges.sql
@@ -6,7 +6,6 @@ CREATE TABLE challenges
     challenge_id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '챌린지 PK',
     user_id      BIGINT                              NOT NULL COMMENT '회원 PK',
     journey_id   BIGINT                              NOT NULL COMMENT '여정 PK',
-    report_id    BIGINT                              NOT NULL COMMENT '성장 리포트 PK',
     content      varchar(500)                        NOT NULL COMMENT '챌린지 설명',
     created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성일',
     updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일'
@@ -20,7 +19,3 @@ ALTER TABLE challenges
 -- changeset Seungwon-Choi:31
 ALTER TABLE challenges
     ADD CONSTRAINT fk_challenges_journey_id FOREIGN KEY (journey_id) REFERENCES journeys (journey_id);
-
--- changeset Seungwon-Choi:32
-ALTER TABLE challenges
-    ADD CONSTRAINT fk_challenges_report_id FOREIGN KEY (report_id) REFERENCES growth_reports (report_id);
