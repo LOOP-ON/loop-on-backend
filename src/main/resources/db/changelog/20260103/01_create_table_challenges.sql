25ed049d2b4e85a770743588663bf7f3f4ab035c
Seungwon-Choi <bestwondev@gmail.com>
2026-01-05 20:56:42 +0900

    refactor: challenges 테이블 컬럼 대문자로 변경

--- src/main/resources/db/changelog/20260103/01_create_table_challenges.sql
+++ src/main/resources/db/changelog/20260103/01_create_table_challenges.sql
@@ -6,7 +6,7 @@ CREATE TABLE challenges
     challenge_id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '챌린지 PK',
     user_id      BIGINT                              NOT NULL COMMENT '회원 PK',
     journey_id   BIGINT                              NOT NULL COMMENT '여정 PK',
-    content      varchar(500)                        NOT NULL COMMENT '챌린지 설명',
+    content      VARCHAR(500)                        NOT NULL COMMENT '챌린지 설명',
     created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성일',
     updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일'
 
