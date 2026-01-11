4c50e1bbe1c4e7fe39451b03cfb78c365f65b52e
Seungwon-Choi <bestwondev@gmail.com>
2026-01-12 02:02:13 +0900

    refactor: PrincipalDetails 수정

--- src/main/java/com/loopon/global/security/principal/PrincipalDetails.java
+++ src/main/java/com/loopon/global/security/principal/PrincipalDetails.java
@@ -8,40 +8,28 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map;
 
 @Getter
 public class PrincipalDetails implements UserDetails {
 
     private final User user;
-    private final Map<String, Object> attributes;
 
     private PrincipalDetails(User user) {
         this.user = user;
-        this.attributes = Map.of();
-    }
-
-    private PrincipalDetails(User user, Map<String, Object> attributes) {
-        this.user = user;
-        this.attributes = attributes;
     }
 
     public static PrincipalDetails of(User user) {
         return new PrincipalDetails(user);
     }
 
-    public static PrincipalDetails of(User user, Map<String, Object> attributes) {
-        return new PrincipalDetails(user, attributes);
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getUserRole()));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
