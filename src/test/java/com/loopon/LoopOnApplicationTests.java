235d4fa8b0bf5b3eb71051f7d0bd24b63fd8d985
Seungwon-Choi <bestwondev@gmail.com>
2026-01-14 21:33:02 +0900

    test: contextLoads 메서드 제거

--- src/test/java/com/loopon/LoopOnApplicationTests.java
+++ src/test/java/com/loopon/LoopOnApplicationTests.java
@@ -1,13 +1,8 @@
 package com.loopon;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LoopOnApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
