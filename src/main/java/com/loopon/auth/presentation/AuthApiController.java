aa4d134c87085a1dfe9c37e94e039e4f3a98c6bb
Seungwon-Choi <bestwondev@gmail.com>
2026-01-17 04:27:11 +0900

    feat: AuthApiDocs 추가

--- src/main/java/com/loopon/auth/presentation/AuthApiController.java
+++ src/main/java/com/loopon/auth/presentation/AuthApiController.java
@@ -5,6 +5,7 @@ import com.loopon.auth.application.dto.request.LoginRequest;
 import com.loopon.auth.application.dto.response.AccessTokenResponse;
 import com.loopon.auth.application.dto.response.LoginSuccessResponse;
 import com.loopon.auth.application.dto.response.ReissueTokensResponse;
+import com.loopon.auth.presentation.docs.AuthApiDocs;
 import com.loopon.global.domain.dto.CommonResponse;
 import com.loopon.global.security.jwt.TokenCookieFactory;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,16 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class AuthApiController {
+public class AuthApiController implements AuthApiDocs {
     private final AuthService authService;
 
     private final TokenCookieFactory tokenCookieFactory;
 
+    @Override
     @PostMapping("/login")
     public ResponseEntity<CommonResponse<LoginSuccessResponse>> login(LoginRequest request) {
         throw new UnsupportedOperationException("이 메서드는 Spring Security의 JsonLoginProcessingFilter에서 처리됩니다.");
     }
 
+    @Override
     @PostMapping("/reissue")
     public ResponseEntity<CommonResponse<AccessTokenResponse>> reissueTokens(
             @CookieValue(value = "refresh_token", required = true) String refreshToken,
@@ -38,13 +41,14 @@ public class AuthApiController {
         ReissueTokensResponse reissueTokensResponse = authService.reissueTokens(refreshToken);
 
         ResponseCookie refreshTokenCookie = tokenCookieFactory.createRefreshTokenCookie(reissueTokensResponse.refreshToken());
-        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
                 .body(CommonResponse.onSuccess(AccessTokenResponse.of(reissueTokensResponse.accessToken())));
     }
 
+    @Override
     @PostMapping("/logout")
     public ResponseEntity<CommonResponse<Void>> logout(
             @CookieValue(value = "refresh_token", required = false) String refreshToken
