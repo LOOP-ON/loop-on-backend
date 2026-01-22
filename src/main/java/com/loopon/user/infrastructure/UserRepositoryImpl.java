35602d1d606e46673d79270eaa54a91db8d1d582
starvingorange <kyungjjoo@gmail.com>
2026-01-22 17:42:17 +0900

    fix: 에러 수정

--- src/main/java/com/loopon/user/infrastructure/UserRepositoryImpl.java
+++ src/main/java/com/loopon/user/infrastructure/UserRepositoryImpl.java
@@ -5,11 +5,8 @@ import com.loopon.global.exception.BusinessException;
 import com.loopon.user.domain.User;
 import com.loopon.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
@@ -45,10 +42,9 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public User findById(Long id) {
         return userJpaRepository.findById(id)
-                .orElseThrow(() -> {
-                    log.error("UserRepositoryImpl.findById - 사용자 없음(id: {})", id);
-                    return new BusinessException(ErrorCode.USER_NOT_FOUND);
-                });
+                .orElseThrow(() ->
+                    new UsernameNotFoundException("해당 이메일의 사용자가 존재하지 않습니다.")
+                );
     }
 
     @Override
