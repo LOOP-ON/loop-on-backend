c139739f5b555b280215e37a32b9fc4211e6a2f0
Seungwon-Choi <bestwondev@gmail.com>
2026-01-24 19:19:09 +0900

    feat: PasswordResetRequest 메시지 추가

--- src/main/java/com/loopon/auth/application/dto/request/PasswordResetRequest.java
+++ src/main/java/com/loopon/auth/application/dto/request/PasswordResetRequest.java
@@ -8,7 +8,7 @@ public record PasswordResetRequest(
         @NotBlank String resetToken,
 
         @NotBlank
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$", message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8~20자여야 합니다.")
         String newPassword,
 
         @NotBlank String confirmPassword
