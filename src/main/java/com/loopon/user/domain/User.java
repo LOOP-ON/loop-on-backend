d0c69f1033de9405dfa405e5256f25687e70000a
Seungwon-Choi <bestwondev@gmail.com>
2026-01-25 20:44:50 +0900

    refactor: Builder 접근 레벨 변경

--- src/main/java/com/loopon/user/domain/User.java
+++ src/main/java/com/loopon/user/domain/User.java
@@ -31,7 +31,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder(access = AccessLevel.PRIVATE)
+@Builder
 public class User extends BaseTimeEntity {
 
     @Id
