dc10d3146a4b86f84eb55a7456ca96a749aaf007
jaeho4125@gmail.com <jaeho4125@gmail.com>
2026-01-31 21:11:04 +0900

    feat: comment_like table 추가

--- src/main/java/com/loopon/challenge/domain/CommentLike.java
+++ src/main/java/com/loopon/challenge/domain/CommentLike.java
@@ -1,47 +1,32 @@
 package com.loopon.challenge.domain;
 
 import com.loopon.user.domain.User;
-import lombok.AllArgsConstructor;
-
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "comments")
+@Table(name = "comment_likes")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
-public class Comment {
+public class CommentLike {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "comment_id")
+    @Column(name = "comment_like_id")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    private Comment parent;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "challenge_id", nullable = false)
-    private Challenge challenge;
-
-    @Column(nullable = false, length = 500)
-    private String content;
-
-    @Column(nullable = false)
-    private Integer likeCount = 0;
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
 
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-    @Column
-    private LocalDateTime updatedAt;
 }
