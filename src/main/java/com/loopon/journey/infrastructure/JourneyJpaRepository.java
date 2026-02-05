e0477f996ff445a3691d5df0643b3d1925bd8541
Seungwon-Choi <bestwondev@gmail.com>
2026-02-05 23:02:07 +0900

    feat: JourneyJpaRepository 메서드 선언 제거

--- src/main/java/com/loopon/journey/infrastructure/JourneyJpaRepository.java
+++ src/main/java/com/loopon/journey/infrastructure/JourneyJpaRepository.java
@@ -1,6 +1,5 @@
 package com.loopon.journey.infrastructure;
 
-import com.loopon.Routine.domain.Routine;
 import com.loopon.journey.domain.Journey;
 import com.loopon.journey.domain.JourneyStatus;
 import com.loopon.user.domain.User;
@@ -9,8 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface JourneyJpaRepository extends JpaRepository<Journey, Long> {
-    Optional<Journey> findById(Long journeyId);
-
     Optional<Journey> findByUserAndStatus(User user, JourneyStatus status);
-
 }
