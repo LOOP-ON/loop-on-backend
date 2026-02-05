860d4e79fd471ffffd4c26fe02063f8dcc3c8cdb
jangyeeunee <vkfkdto2053@gmail.com>
2026-02-05 16:29:24 +0900

    feat: 루틴 미루기

---
+++ src/main/java/com/loopon/Routine/infrastructure/RoutineProgressJpaRepository.java
@@ -0,0 +1,15 @@
+package com.loopon.Routine.infrastructure;
+
+import com.loopon.Routine.domain.Routine;
+import com.loopon.Routine.domain.RoutineProgress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface RoutineProgressJpaRepository extends JpaRepository<RoutineProgress, Long> {
+    Optional<RoutineProgress> findByRoutineAndProgressDate(
+            Routine routine,
+            LocalDate progressDate
+    );
+}
