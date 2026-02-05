fb49826a1918bb72191d6b59ba6bb156adef5fb8
jaeho4125@gmail.com <jaeho4125@gmail.com>
2026-02-05 16:59:07 +0900

    feat: ChallengeApiDocs 추가

--- src/main/java/com/loopon/challenge/presentation/ChallengeApiController.java
+++ src/main/java/com/loopon/challenge/presentation/ChallengeApiController.java
@@ -2,25 +2,17 @@ package com.loopon.challenge.presentation;
 
 import com.loopon.challenge.application.converter.ChallengeConverter;
 import com.loopon.challenge.application.dto.command.*;
-import com.loopon.challenge.application.dto.request.ChallengeCommentRequest;
-import com.loopon.challenge.application.dto.request.ChallengeLikeRequest;
-import com.loopon.challenge.application.dto.request.ChallengeModifyRequest;
+import com.loopon.challenge.application.dto.request.*;
 import com.loopon.challenge.application.dto.response.*;
-import com.loopon.challenge.application.dto.request.ChallengePostRequest;
 import com.loopon.challenge.application.service.ChallengeCommandService;
 import com.loopon.challenge.application.service.ChallengeQueryService;
 import com.loopon.challenge.presentation.docs.ChallengeApiDocs;
 import com.loopon.global.domain.dto.CommonResponse;
 import com.loopon.global.security.principal.PrincipalDetails;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
@@ -28,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -38,180 +29,131 @@ public class ChallengeApiController implements ChallengeApiDocs {
     private final ChallengeCommandService challengeCommandService;
     private final ChallengeQueryService challengeQueryService;
 
+    @Override
     @PostMapping(value = "/api/challenges", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public CommonResponse<ChallengePostResponse> postChallenge(
-            @RequestPart("requestDto")
-            @Valid
-            @Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
             ChallengePostRequest requestDto,
-
-            @RequestPart("imageFiles")
-            @Size(min = 1, max = 10, message = "사진은 최대 10개까지 가능합니다.")
-            @Parameter(content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
             List<MultipartFile> imageFiles,
-
-            @AuthenticationPrincipal
-            PrincipalDetails principalDetails
+            @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
         ChallengePostCommand commandDto =
                 ChallengeConverter.postChallenge(requestDto, imageFiles, principalDetails.getUserId());
 
-        return CommonResponse.onSuccess(
-                challengeCommandService.postChallenge(commandDto)
-        );
+        return CommonResponse.onSuccess(challengeCommandService.postChallenge(commandDto));
     }
 
+    @Override
     @GetMapping("/api/challenges/{challengeId}")
-    public CommonResponse<ChallengeGetResponse> getChallenge (
+    public CommonResponse<ChallengeGetResponse> getChallenge(
             @PathVariable Long challengeId
     ) {
-        return CommonResponse.onSuccess(
-                challengeQueryService.getChallenge(challengeId)
-        );
+        return CommonResponse.onSuccess(challengeQueryService.getChallenge(challengeId));
     }
 
+    @Override
     @PostMapping("/api/challenges/{challengeId}/like")
-    public CommonResponse<ChallengeLikeResponse> likeChallenge (
+    public CommonResponse<ChallengeLikeResponse> likeChallenge(
             @PathVariable Long challengeId,
             @RequestBody ChallengeLikeRequest requestDto,
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        ChallengeLikeCommand commandDto
-                = ChallengeConverter.likeChallenge(challengeId, requestDto, principalDetails);
-
-        return CommonResponse.onSuccess(
-                challengeCommandService.likeChallenge(commandDto)
-        );
+        ChallengeLikeCommand commandDto = ChallengeConverter.likeChallenge(challengeId, requestDto, principalDetails);
+        return CommonResponse.onSuccess(challengeCommandService.likeChallenge(commandDto));
     }
 
+    @Override
     @PatchMapping(value = "/api/challenges/{challengeId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public CommonResponse<ChallengeModifyResponse> patchChallenge (
-            @RequestPart("requestDto")
-            @Valid
-            @Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+    public CommonResponse<ChallengeModifyResponse> patchChallenge(
             ChallengeModifyRequest requestDto,
-
-            @RequestPart(value = "imageFiles", required = false)
-            @Size(max = 10, message = "사진은 최대 10개까지 가능합니다.")
-            @Parameter(content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
             List<MultipartFile> imageFiles,
-
             @PathVariable Long challengeId
     ) {
-        ChallengeModifyCommand commandDto =
-                ChallengeConverter.modifyChallenge(requestDto, challengeId, imageFiles);
-
-        return CommonResponse.onSuccess(
-                challengeCommandService.modifyChallenge(commandDto)
-        );
+        ChallengeModifyCommand commandDto = ChallengeConverter.modifyChallenge(requestDto, challengeId, imageFiles);
+        return CommonResponse.onSuccess(challengeCommandService.modifyChallenge(commandDto));
     }
 
+    @Override
     @PostMapping("/api/challenges/{challengeId}/comments")
-    public CommonResponse<ChallengeCommentResponse> commentChallenge (
+    public CommonResponse<ChallengeCommentResponse> commentChallenge(
             @PathVariable Long challengeId,
             @RequestBody ChallengeCommentRequest requestDto,
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        ChallengeCommentCommand commandDto =
-                ChallengeConverter.commentChallenge(challengeId, requestDto, principalDetails.getUserId());
-
-        return CommonResponse.onSuccess(
-                challengeCommandService.commentChallenge(commandDto)
-        );
+        ChallengeCommentCommand commandDto = ChallengeConverter.commentChallenge(challengeId, requestDto, principalDetails.getUserId());
+        return CommonResponse.onSuccess(challengeCommandService.commentChallenge(commandDto));
     }
 
+    @Override
     @GetMapping("/api/challenges/{challengeId}/comments")
-    public CommonResponse<Slice<ChallengeGetCommentResponse>> getCommentChallenge (
+    public CommonResponse<Slice<ChallengeGetCommentResponse>> getCommentChallenge(
             @PathVariable Long challengeId,
-            @PageableDefault Pageable pageable
+            Pageable pageable
     ) {
-        ChallengeGetCommentCommand commandDto
-                = ChallengeConverter.getCommentChallenge(challengeId, pageable);
-
-        return CommonResponse.onSuccess(
-                challengeQueryService.getCommentChallenge(commandDto)
-        );
+        ChallengeGetCommentCommand commandDto = ChallengeConverter.getCommentChallenge(challengeId, pageable);
+        return CommonResponse.onSuccess(challengeQueryService.getCommentChallenge(commandDto));
     }
 
+    @Override
     @PostMapping("/api/challenges/comment/{commentId}/like")
-    public CommonResponse<ChallengeLikeCommentResponse> likeCommentChallenge (
+    public CommonResponse<ChallengeLikeCommentResponse> likeCommentChallenge(
             @PathVariable Long commentId,
+            @RequestBody ChallengeLikeCommentRequest requestDto,
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        ChallengeLikeCommentCommand commandDto
-                = ChallengeConverter.likeCommentChallenge(commentId, principalDetails.getUserId());
-
-        return CommonResponse.onSuccess(
-                challengeCommandService.likeCommentChallenge(commandDto)
-        );
+        ChallengeLikeCommentCommand commandDto = ChallengeConverter.likeCommentChallenge(commentId, requestDto, principalDetails.getUserId());
+        return CommonResponse.onSuccess(challengeCommandService.likeCommentChallenge(commandDto));
     }
 
+    @Override
     @DeleteMapping("/api/challenges/{challengeId}/comments/{commentId}")
-    public CommonResponse<Void> deleteCommentChallenge (
+    public CommonResponse<Void> deleteCommentChallenge(
             @PathVariable Long commentId,
             @PathVariable Long challengeId,
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        ChallengeDeleteCommentCommand commandDto
-                = ChallengeConverter.deleteCommentChallenge(commentId, challengeId, principalDetails.getUserId());
-
-        return CommonResponse.onSuccess(
-                challengeCommandService.deleteCommentChallenge(commandDto)
-        );
+        ChallengeDeleteCommentCommand commandDto = ChallengeConverter.deleteCommentChallenge(commentId, challengeId, principalDetails.getUserId());
+        return CommonResponse.onSuccess(challengeCommandService.deleteCommentChallenge(commandDto));
     }
 
+    @Override
     @DeleteMapping("/api/challenges/{challengeId}")
-    public CommonResponse<Void> deleteChallenge (
+    public CommonResponse<Void> deleteChallenge(
             @PathVariable Long challengeId,
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        ChallengeDeleteCommand commandDto
-                = ChallengeConverter.deleteChallenge(challengeId, principalDetails.getUserId());
-
-        return CommonResponse.onSuccess(
-                challengeCommandService.deleteChallenge(commandDto)
-        );
+        ChallengeDeleteCommand commandDto = ChallengeConverter.deleteChallenge(challengeId, principalDetails.getUserId());
+        return CommonResponse.onSuccess(challengeCommandService.deleteChallenge(commandDto));
     }
 
+    @Override
     @GetMapping("/api/challenges/users/me")
-    public CommonResponse<Slice<ChallengePreviewResponse>> myChallenge (
+    public CommonResponse<Slice<ChallengePreviewResponse>> myChallenge(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
-            @PageableDefault Pageable pageable
+            Pageable pageable
     ) {
-        ChallengeMyCommand commandDto
-                = ChallengeConverter.myChallenge(principalDetails.getUserId(), pageable);
-
-        return CommonResponse.onSuccess(
-                challengeQueryService.myChallenge(commandDto)
-        );
+        ChallengeMyCommand commandDto = ChallengeConverter.myChallenge(principalDetails.getUserId(), pageable);
+        return CommonResponse.onSuccess(challengeQueryService.myChallenge(commandDto));
     }
 
+    @Override
     @GetMapping("/api/challenges/users/{nickname}")
-    public CommonResponse<Slice<ChallengePreviewResponse>> othersChallenge (
+    public CommonResponse<Slice<ChallengePreviewResponse>> othersChallenge(
             @PathVariable String nickname,
-            @PageableDefault Pageable pageable,
+            Pageable pageable,
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        ChallengeOthersCommand commandDto
-                = ChallengeConverter.othersChallenge(principalDetails.getUserId(), nickname, pageable);
-
-        return CommonResponse.onSuccess(
-                challengeQueryService.othersChallenge(commandDto)
-        );
+        ChallengeOthersCommand commandDto = ChallengeConverter.othersChallenge(principalDetails.getUserId(), nickname, pageable);
+        return CommonResponse.onSuccess(challengeQueryService.othersChallenge(commandDto));
     }
 
+    @Override
     @GetMapping("/api/challenges")
-    public CommonResponse<ChallengeCombinedViewResponse> viewChallenge (
+    public CommonResponse<ChallengeCombinedViewResponse> viewChallenge(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
-            @PageableDefault @Qualifier("trending") Pageable trendingPageable,
-            @PageableDefault @Qualifier("friends") Pageable friendsPageable
-    ){
-        ChallengeViewCommand commandDto
-                = ChallengeConverter.viewChallenge(principalDetails.getUserId(), trendingPageable, friendsPageable);
-
-        return CommonResponse.onSuccess(
-                challengeQueryService.viewChallenge(commandDto)
-        );
+            @Qualifier("trending") Pageable trendingPageable,
+            @Qualifier("friends") Pageable friendsPageable
+    ) {
+        ChallengeViewCommand commandDto = ChallengeConverter.viewChallenge(principalDetails.getUserId(), trendingPageable, friendsPageable);
+        return CommonResponse.onSuccess(challengeQueryService.viewChallenge(commandDto));
     }
-
 }
--- src/main/java/com/loopon/challenge/presentation/docs/ChallengeApiDocs.java
+++ src/main/java/com/loopon/challenge/presentation/docs/ChallengeApiDocs.java
@@ -1,101 +1,123 @@
 package com.loopon.challenge.presentation.docs;
 
-import com.loopon.challenge.application.dto.request.ChallengeCommentRequest;
-import com.loopon.challenge.application.dto.request.ChallengeLikeRequest;
-import com.loopon.challenge.application.dto.request.ChallengeModifyRequest;
-import com.loopon.challenge.application.dto.request.ChallengePostRequest;
+import com.loopon.challenge.application.dto.request.*;
 import com.loopon.challenge.application.dto.response.*;
 import com.loopon.global.domain.dto.CommonResponse;
 import com.loopon.global.security.principal.PrincipalDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
-@Tag(name = "챌린지(Challenge)", description = "챌린지, 댓글 및 좋아요 관련 API")
+@Tag(name = "8. 챌린지(Challenge)", description = "챌린지, 댓글 및 좋아요 관련 API")
 public interface ChallengeApiDocs {
 
-    @Operation(summary = "챌린지 업로드", description = "이미지와 내용을 포함한 새로운 챌린지를 게시합니다. (Multipart/form-data)")
+    @Operation(summary = "챌린지 업로드")
     CommonResponse<ChallengePostResponse> postChallenge(
-            @RequestBody(description = "챌린지 게시글 정보 (JSON)") ChallengePostRequest requestDto,
-            @Parameter(description = "인증 이미지 파일 목록 (최대 10개)") List<MultipartFile> imageFiles,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
+            @RequestPart("requestDto")
+            @Valid
+            @Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+            ChallengePostRequest requestDto,
+
+            @RequestPart("imageFiles")
+            @Size(min = 1, max = 10, message = "사진은 최대 10개까지 가능합니다.")
+            @Parameter(content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
+            List<MultipartFile> imageFiles,
+
+            PrincipalDetails principalDetails
     );
 
-    @Operation(summary = "챌린지 업로드 상세 조회", description = "수정을 위해 업로드된 특정 챌린지의 상세 정보를 조회합니다.")
+    @Operation(summary = "챌린지 업로드 상세 조회")
     CommonResponse<ChallengeGetResponse> getChallenge(
-            @Parameter(description = "챌린지 ID") Long challengeId
+            @PathVariable("challengeId") Long challengeId
     );
 
-    @Operation(summary = "챌린지 좋아요/취소", description = "챌린지 게시글에 좋아요를 누르거나 취소합니다.")
+    @Operation(summary = "챌린지 좋아요/취소")
     CommonResponse<ChallengeLikeResponse> likeChallenge(
-            @Parameter(description = "챌린지 ID") Long challengeId,
-            @RequestBody(description = "좋아요 상태 정보") ChallengeLikeRequest requestDto,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
+            @PathVariable("challengeId") Long challengeId,
+            ChallengeLikeRequest requestDto,
+            PrincipalDetails principalDetails
     );
 
-    @Operation(summary = "챌린지 수정", description = "기존 챌린지의 내용이나 이미지를 수정합니다.")
+    @Operation(summary = "챌린지 수정")
     CommonResponse<ChallengeModifyResponse> patchChallenge(
-            @RequestBody(description = "수정할 내용 (JSON)") ChallengeModifyRequest requestDto,
-            @Parameter(description = "새로 업로드할 이미지 파일 목록") List<MultipartFile> imageFiles,
-            @Parameter(description = "챌린지 ID") Long challengeId
+            @RequestPart("requestDto")
+            @Valid
+            @Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+            ChallengeModifyRequest requestDto,
+
+            @RequestPart(value = "imageFiles", required = false)
+            @Size(max = 10, message = "사진은 최대 10개까지 가능합니다.")
+            @Parameter(content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
+            List<MultipartFile> imageFiles,
+
+            @PathVariable("challengeId")
+            Long challengeId
     );
 
-    @Operation(summary = "챌린지 댓글 작성", description = "챌린지 게시글에 댓글을 작성합니다.")
+    @Operation(summary = "챌린지 댓글 작성")
     CommonResponse<ChallengeCommentResponse> commentChallenge(
-            @Parameter(description = "챌린지 ID") Long challengeId,
-            @RequestBody(description = "댓글 내용") ChallengeCommentRequest requestDto,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
+            @PathVariable("challengeId") Long challengeId,
+            @Valid ChallengeCommentRequest requestDto,
+            PrincipalDetails principalDetails
     );
 
-    @Operation(summary = "챌린지 댓글 목록 조회", description = "특정 챌린지에 달린 댓글들을 페이징하여 조회합니다.")
+    @Operation(summary = "챌린지 댓글 목록 조회")
     CommonResponse<Slice<ChallengeGetCommentResponse>> getCommentChallenge(
-            @Parameter(description = "챌린지 ID") Long challengeId,
-            Pageable pageable
+            @PathVariable("challengeId") Long challengeId,
+            @PageableDefault Pageable pageable
     );
 
-    @Operation(summary = "댓글 좋아요/취소", description = "댓글에 좋아요를 누르거나 취소합니다.")
+    @Operation(summary = "댓글 좋아요/취소")
     CommonResponse<ChallengeLikeCommentResponse> likeCommentChallenge(
-            @Parameter(description = "댓글 ID") Long commentId,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
+            @PathVariable("commentId") Long commentId,
+            @Valid ChallengeLikeCommentRequest requestDto,
+            PrincipalDetails principalDetails
     );
 
-    @Operation(summary = "댓글 삭제", description = "작성한 댓글을 삭제합니다.")
+    @Operation(summary = "댓글 삭제")
     CommonResponse<Void> deleteCommentChallenge(
-            @Parameter(description = "댓글 ID") Long commentId,
-            @Parameter(description = "챌린지 ID") Long challengeId,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
+            @PathVariable("commentId") Long commentId,
+            @PathVariable("challengeId") Long challengeId,
+            PrincipalDetails principalDetails
     );
 
-    @Operation(summary = "챌린지 삭제", description = "본인이 작성한 챌린지를 삭제합니다.")
+    @Operation(summary = "챌린지 삭제")
     CommonResponse<Void> deleteChallenge(
-            @Parameter(description = "챌린지 ID") Long challengeId,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
+            @PathVariable("challengeId") Long challengeId,
+            PrincipalDetails principalDetails
     );
 
-    @Operation(summary = "내 챌린지 모아보기", description = "내가 작성한 챌린지 목록의 미리보기를 조회합니다.")
+    @Operation(summary = "내 챌린지 모아보기")
     CommonResponse<Slice<ChallengePreviewResponse>> myChallenge(
-            @AuthenticationPrincipal PrincipalDetails principalDetails,
-            Pageable pageable
+            PrincipalDetails principalDetails,
+            @PageableDefault Pageable pageable
     );
 
-    @Operation(summary = "타인의 챌린지 모아보기", description = "특정 닉네임을 가진 사용자의 챌린지 목록의 미리보기를 조회합니다.")
+    @Operation(summary = "타인의 챌린지 모아보기")
     CommonResponse<Slice<ChallengePreviewResponse>> othersChallenge(
-            @Parameter(description = "상대방 닉네임") String nickname,
-            Pageable pageable,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
+            @PathVariable("nickname") String nickname,
+            @PageableDefault Pageable pageable,
+            PrincipalDetails principalDetails
     );
 
-    @Operation(summary = "챌린지 홈 피드 조회", description = "트렌딩(인기) 챌린지와 친구들의 챌린지를 동시에 조회합니다.")
+    @Operation(summary = "챌린지 홈 피드 조회. 트렌딩 챌린지와 친구 챌린지의 비율은 기본적으로 1:3을 유지합니다.")
     CommonResponse<ChallengeCombinedViewResponse> viewChallenge(
-            @AuthenticationPrincipal PrincipalDetails principalDetails,
-            @Parameter(hidden = true) Pageable trendingPageable,
-            @Parameter(hidden = true) Pageable friendsPageable
+            PrincipalDetails principalDetails,
+            @PageableDefault @Qualifier("trending") Pageable trendingPageable,
+            @PageableDefault @Qualifier("friends") Pageable friendsPageable
     );
 }
