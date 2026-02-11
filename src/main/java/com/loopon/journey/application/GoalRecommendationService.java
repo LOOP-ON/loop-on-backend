bc9ded97d8f98d9d736e643c6c71eaad08e3914b
Seungwon-Choi <bestwondev@gmail.com>
2026-02-11 19:07:57 +0900

    refactor: 목표 생성 관련 서비스 클래스명 변경

--- src/main/java/com/loopon/journey/application/GoalRecommendationService.java
+++ src/main/java/com/loopon/journey/application/GoalRecommendationService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class JourneyRecommendationService {
+public class GoalRecommendationService {
 
     private final LLMApplicationServiceImpl llmService;
 
--- src/main/java/com/loopon/journey/application/service/GoalRegenerationService.java
+++ src/main/java/com/loopon/journey/application/service/GoalRegenerationService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class LoopRegenerationService {
+public class GoalRegenerationService {
 
     private final LLMApplicationServiceImpl llmService;
 
